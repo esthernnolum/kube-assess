52fae1927e882a99e688c4e6dba95fd8f4958539
Nnolum Esther <esthernnolum@gmail.com>
2023-09-21 07:01:57 +0100

    update file

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # bash script that builds, packages and deploys the python apllication aslo take care of configuring the minikube cluster, and ensuring the webserver endpoint is available.
 
 eval $(minikube -p minikube docker-env)
-docker build -t kube-task .
+docker build -t webservice2 .
 
 
 minikube addons enable ingress
@@ -12,3 +12,5 @@ kubectl apply -f webservice1.yml
 kubectl apply -f webservice2.yml
 kubectl apply -f kube-secret.yml
 kubectl rollout status deployment/webservice2-deploy
+# Add Ingress host to /etc/hosts
+echo "$(minikube ip) webservice2.local" | sudo tee -a /etc/hosts
